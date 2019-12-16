01f180ebc028a684ae0159ae696ebf4f6fabd70e
Zak <zakadmaster@gmail.com>
2019-12-16 14:00:00 +0100

    Addition of getCarInfoById functionalities

--- index.js
+++ index.js
@@ -100,27 +100,27 @@ function makeSmartPerson(nameArg) {
 
 
 
-/*
+
 // ⭐️ Example Test Data ⭐️
 
-var inventory = [
-  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+// var inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
   /// ... Truncated
-]
-*/
+// ]
+
 /**
   * ### Example Array Challenge:
   * 
@@ -184,9 +184,10 @@ function getLastCarInfo(inventoryArray) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoById(/* code here */) {
-  /* code here */
+function getCarInfoById(inventory, idNum) {
+  return "This is a " + inventory[idNum - 1].car_make + " " + inventory[idNum - 1].car_model;
 }
+
 
 /**
  * ### Challenge `sortCarInventory`
